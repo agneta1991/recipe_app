1c106eaf840d021cb2694f6e06ce046802b48d4a
agneta1991 <pupienytea@gmail.com>
2023-11-13 14:42:59 +0200

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: %i(index)
-  resources :foods, only: %i(index)
-  resources :recipes, only: %i(index, show)
+  resources :foods, only: %i(index show)
+  resources :recipes, only: %i(index show)
 end
