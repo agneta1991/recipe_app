c2cb61b30e8362e893dd25053a70fd555ac1db02
agneta1991 <pupienytea@gmail.com>
2023-11-14 13:02:52 +0200

    added logic to add food to the data base and delete it

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: %i(index)
-  resources :foods, only: %i(index show)
+  resources :foods, only: %i(index show new create destroy)
   resources :recipes, only: %i(index show)
+
+  root to: 'recipes#index'
 end
