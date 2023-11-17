7e3b92eac4c9ade4e1c20a2ea3d023a5a62b9ad8
agneta1991 <pupienytea@gmail.com>
2023-11-17 17:41:48 +0200

    All required changes have been added

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,9 +1,5 @@
 class PublicRecipesController < ApplicationController
   def index
-    @public_recipes = if user_signed_in?
-                        current_user.recipes.or(Recipe.where(public: true)).order(created_at: :desc)
-                      else
-                        Recipe.where(public: true).order(created_at: :desc)
-                      end
+    @public_recipes = Recipe.where(public: true).order(created_at: :desc)
   end
 end
