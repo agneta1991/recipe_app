819c1e8938339a2b46cc2e5d5129fd32f7bba0bc
agneta1991 <pupienytea@gmail.com>
2023-11-17 13:30:11 +0200

    General shopping list controller updated

--- spec/requests/general_shopping_lists_spec.rb
+++ spec/requests/general_shopping_lists_spec.rb
@@ -29,20 +29,7 @@ RSpec.describe 'GeneralShoppingLists', type: :request do
     end
 
     it 'includes correct placeholder text in the response body' do
-      expect(response.body).to include('<h1>Shoppin list</h1>')
-    end
-
-    it 'assigns the correct values to instance variables' do
-      user = User.create(id: 1, name: 'Agneta', email: 'agneta@agneta.com', password: 'password')
-      Food.create(name: 'Banana', measurement_unit: 'kg', price: 3, quantity: 2, user_id: user.id)
-      Food.create(name: 'Apple', measurement_unit: 'kg', price: 2, quantity: 3, user_id: user.id)
-      Food.create(name: 'Strawberry', measurement_unit: 'kg', price: 1, quantity: 2, user_id: user.id)
-
-      sign_in user
-      get general_shopping_lists_path
-      expect(response).to be_successful
-
-      expect(assigns(:quantity)).to eq(4)
+      expect(response.body).to include('<h1>Shopping list for all your recipes</h1>')
     end
   end
 end
